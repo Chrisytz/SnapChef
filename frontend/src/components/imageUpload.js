a8107e1bf5b7f8764681413ada12fa8cafb217b9
edward-song-life-is-short <edwarddsongg@gmail.com>
2024-05-17 13:56:04 -0400

    add cohere support

--- frontend/src/components/imageUpload.js
+++ frontend/src/components/imageUpload.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 import {createGoal} from "../features/goals/goalSlice";
 import {useDispatch} from "react-redux";
 import "../styles/imageUpload.css"
-
+import cohere from 'cohere-ai';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faAngleLeft} from '@fortawesome/free-solid-svg-icons';
 import { faUpload} from '@fortawesome/free-solid-svg-icons';
@@ -32,24 +32,28 @@ function ImageUpload() {
     const openai = new OpenAI({apiKey: process.env.REACT_APP_GPT_API_KEY, dangerouslyAllowBrowser: true});
     const dispatch = useDispatch()
 
-    async function getRecipe(ingredients) {
-        const completion = await openai.chat.completions.create({
-            messages: [
-                { role: "system", content:
-                        "you are a chef who is going to give out simple recipes with ingredients and steps based on a list of ingredients the user will provide in JSON format." +
-                        "Please also add the necessary spices and ingredients, make sure the ones you add are common in households" +
-                        "Here are the specifications of the formatting." +
-                        "The name of the recipe should be a key value pair with the key being \"recipe_name\"" +
-                        "The ingredients of the recipe should be in a nested dictionary with key \"ingredients\" and inside that nested dictionary" +
-                        "each ingredient should have the key be the ingredient name and the value be the quantity/details" +
-                        "finally the steps to prepare the recipe should be in a list with key \"steps\""},
-                { role: "user", content: ingredients}],
-            model: "gpt-3.5-turbo-1106",
-            response_format: { type: "json_object" },
-        });
+    cohere.init(process.env.COHERE_API_KEY);
 
-        console.log(completion.choices[0].message.content);
-        return completion.choices[0].message.content
+    async function getRecipe(ingredients) {
+        const response = await cohere.generate({
+            model: 'xlarge',
+            prompt: `
+            You are a chef who is going to give out simple recipes with ingredients and steps based on a list of ingredients the user will provide in JSON format.
+            Please also add the necessary spices and ingredients, make sure the ones you add are common in households.
+            Here are the specifications of the formatting:
+            - The name of the recipe should be a key-value pair with the key being "recipe_name".
+            - The ingredients of the recipe should be in a nested dictionary with key "ingredients" and inside that nested dictionary, each ingredient should have the key be the ingredient name and the value be the quantity/details.
+            - Finally, the steps to prepare the recipe should be in a list with the key "steps".
+    
+            Ingredients: ${ingredients}
+            `,
+            max_tokens: 300,
+            temperature: 0.7,
+        });
+    
+        const recipe = response.body.generations[0].text;
+        console.log(recipe);
+        return recipe;
     }
     
     function uploadImage() {
